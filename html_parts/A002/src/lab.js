47fa1ab37dd5ad2d032eb5bf465324e885d80fff
Igor R. Nagumanov <i.nagumanov@pflb.ru>
2017-09-05 18:37:36 +0300

    commit

--- html_parts/A002/src/lab.js
+++ html_parts/A002/src/lab.js
@@ -6,3 +6,10 @@ var name = 'John';  // name of a user
 
 var $ = (5, 6, 7);
 alert($);
+
+var age = prompt('Сколько вам лет?', '');
+if(age < 14) {
+    alert('Пошел отсюда, сопляк!');
+} else { // необязательный блок
+    alert('Дарова брателло!');
+}
